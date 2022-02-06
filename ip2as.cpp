681f4d0481cf126c2dd99529dc63a7f5e1f1f8c6
tunasmoothie <tunasmoothie@gmail.com>
2022-02-05 17:05:05 -0800

    creation

---
+++ ip2as.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv){
+	
+	// cmd line processing
+	if(argc != 3){
+		cout << "\nError: unrecognized or incomplete command line.\n\n" <<
+				"USAGE:\n" <<
+				"    ip2as [DB file] [IP file]\n\n";
+		exit(1);
+	}
+	
+	
+	
+	return 0;
+}
+
+uint32_t ip2binary(string ip, int cidr)
+
+class Node{
+	
+}
+
+class Trie{
+	
+}
